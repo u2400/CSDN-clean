c81f968aa0ba41ab47bec35a21f8da55049b4134
HardToName <>
2018-12-25 13:25:54 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+var ad = $('iframe');
+for (let i in ad)
+{
+    if( ad[i].tagName && ad[i].tagName == "IFRAME" )
+    {
+        if( ad[i].parentNode.tagName == "DIV" )
+        {
+            ad[i].parentNode.style['display'] = "none";
+            if( ad[i].parentNode.parentNode.tagName == "DIV" )
+            {
+                ad[i].parentNode.parentNode.style['display'] = "none";
+            }
+        }
+    }
+}
